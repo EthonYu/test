aa993b39e8a6fbb2aa5a6cf8d084ee9c273e4859
mldn <yubindetiankong@163.com>
2019-03-25 17:01:48 +0800

    Change master Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,5 @@
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello Git1");
-		System.out.println("Hello Git2");
-		System.out.println("Hello Git3");
-		System.out.println("Hello Git4");
+		System.out.println("Hello master main");
 	}
 }
