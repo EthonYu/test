b1b53e6159c61e5e08a7f4cb438954c91153325a
mldn <yubindetiankong@163.com>
2019-03-25 14:56:00 +0800

    add Emp.java

---
+++ Emp.java
@@ -0,0 +1,8 @@
+public class Emp {
+	public static void main(String[] args) {
+		System.out.println("Hello Git1");
+		System.out.println("Hello Git2");
+		System.out.println("Hello Git3");
+		System.out.println("Hello Git4");
+	}
+}
