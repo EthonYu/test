29ceb808e87900a445a7f8d0c65e5164369235b2
mldn <yubindetiankong@163.com>
2019-03-25 14:53:03 +0800

    Add a Emp.java and change Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
 public class Hello {
-	public static void main() {
+	public static void main(String[] args) {
 		System.out.println("Hello Git1");
 		System.out.println("Hello Git2");
 		System.out.println("Hello Git3");
+		System.out.println("Hello Git4");
 	}
 }
