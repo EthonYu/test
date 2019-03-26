bef51dd9e54689300eb7696e6af2a7ceb11622c1
mldn <yubindetiankong@163.com>
2019-03-26 17:22:33 +0800

    test conflict

--- Admin.java
+++ Admin.java
@@ -1,6 +1,8 @@
 public class Admin{
 	private String admin;
 	private String name;
+private Integer id;
+private String test_no;
 	public static void main(String[] args) {
 		System.out.println("Hello Admin")
 	}
