b5268f4138d39476aeb7d09cf9de9d6ab6393895
mldn <yubindetiankong@163.com>
2019-03-26 15:14:23 +0800

    Change Admin.java and Emp.java, add createdb.sql file

--- Admin.java
+++ Admin.java
@@ -1,4 +1,5 @@
 public class Admin{
+	private String admin;
 	public static void main(String[] args) {
 		System.out.println("Hello Admin")
 	}
--- Emp.java
+++ Emp.java
@@ -1,4 +1,6 @@
 public class Emp {
+	private String ename;
+	private String dept;
 	public static void main(String[] args) {
 		System.out.println("Emp1");
 		System.out.println("Emp2");
